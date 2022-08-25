af5c8fd1fa037e02b3b247b77b405f18a7007bfb
SAHILKHAN7001 <106735502+SAHILKHAN7001@users.noreply.github.com>
2022-08-25 11:16:48 +0530

    k

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-let search = document.getElementById("searchInput");
+let searchInput = document.getElementById("searchInput");
 
-search.addEventListener("keyup",function(event) {
-    let searching = event.target.value.toLowerCase();
-    let allList = document.getElementsByClassName("name");
-    for(let i=0; i<allList.length;i++) {
-        const searchName = allList[i].textContent.toLowerCase();
-     
-        if(searchName.includes(searching)) {
-            allList[i].style.display = "block";
+searchInput.addEventListener("keyup",function(event) {
+   let allListNames = document.getElementsByClassName("name");
+   let searching = event.target.value.toLowerCase();
+   for(let k=0; k<allListNames.length;k++) {
+       const loop = allListNames[k].textContent.toLowerCase();
+      
+       if(loop.includes(searching)) {
+           allListNames[k].style.display = "block";
         }
         else {
-            allList[i].style.display = "none";
+            allListNames[k].style.display ="none";
         }
-    }
+   }
 })
